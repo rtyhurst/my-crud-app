7d8059a4efbe9853a1195261cec1f1a07d15d979
rtyhu <rtyhurst@gmail.com>
2024-07-20 17:38:45 -0700

    video = 1:06 Add task management functionality to Flask project
    This update introduces a new feature to manage tasks in the Flask project. A new database is created to overcome the tasks, including the functionality of add, edit, and delete. Updated templates to reflect these new functionalities and implement the flow of task management.

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,15 +1,80 @@
-from flask import Flask, render_template
+from datetime import datetime
+
+from flask import Flask, render_template, redirect, url_for, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 Scss(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+# Data Class - row of data
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Task {self.id}"
+
+
+# Routes to webpages
+# Home page
+@app.route('/', methods=['GET', 'POST'])
 def index():  # put application's code here
-    return render_template('index.html')
+    # add task
+    if request.method == 'POST':
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"ERROR:{e}")
+            return f"ERROR:{e}"
 
+    #     See all current tasks
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+    return render_template('index.html', tasks=tasks)
+
+
+# Delete an Item
+@app.route("/delete/<int:id>")
+def delete(id: int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        return f"ERROR:{e}"
+
+
+# Edit an Item
+@app.route("/edit/<int:id>", methods=['GET', 'POST'])
+def edit(id: int):
+    task = MyTask.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"ERROR:{e}"
+    else:
+        return render_template('edit.html', task=task)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        # task = MyTask(content='Sample Task', complete=0)
+        # db.session.add(task)
+        # db.session.commit()
+
     app.run(debug=True)
